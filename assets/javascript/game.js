a17611d0bbba51518581e33b43d66dd862f60804
Tiny Phantom <shields.bethany@gmail.com>
2019-02-26 13:00:53 -0800

    working game
    logs wins and losses
    log letters guessed
    takes away from guesses left
    resets when guesses left hits 0.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,17 +6,18 @@ var computerChoices = ['a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n'
 var wins= 0;
 var losses= 0;
 var guessesLeft= 10;
-//var wrongGuesses= [];
+var wrongGuesses= [];
 
 
 
-/*function to reset game
+//function to reset game
 function resetGame(){
-    guessesLeft= 'Guesses Left: 10';
-    computerChoice= '';
-    wins= 'Wins: 0'
+    guessesLeft= 10;
+    losses= 0;
+    wins= 0;
+    wrongGuesses= [];
 }
-*/
+
 //function for key up event
 document.onkeyup = function(event){
 //variable for the button pressed by user    
@@ -33,18 +34,24 @@ document.onkeyup = function(event){
 if (userGuess === computerGuess){
     wins++;
     alert("Wow you won!");
+    resetGame();
 } else {
+    wrongGuesses.push(userGuess);
     losses++;
-    alert("Sorry Kiddo!");
     guessesLeft--;
+    console.log(wrongGuesses);
 }
 console.log("WinCount: " + wins + " | LossCount: " + losses + " | NumGuesses: " + guessesLeft);
 //display guesses, wins and losses.
 document.getElementById("wins-text").innerHTML= "Wins: " + wins;
 document.getElementById("loss-text").innerHTML= "Losses: " + losses;
 document.getElementById("guessesLeft-text").innerHTML= "Guesses " + guessesLeft;
-document.getElementById("guessesSoFar-text").innerHTML= "Letters You Guessed: " + userGuess;
+document.getElementById("guessesSoFar-text").innerHTML= "Letters You Guessed: " + wrongGuesses;
 
+if (guessesLeft=== 0){
+    alert('Sorry maybe next time');
+    resetGame();
+}
 };
 
 //need a rest when guesses left reaches 0 giant loop?
